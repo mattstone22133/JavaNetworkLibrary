119f0bdd2b1d569fd8d079a264e30b925ca07be4
Matt Stone <mattstone22133@gmail.com>
2017-03-05 18:45:40 -0500

    forgot to update the test suite for the 5 tests of previous commit, updated. First 5 should pass, remaining 7 should fail (due to not being implemented).

--- enigma/engine/network/test/AllTests.java
+++ enigma/engine/network/test/AllTests.java
@@ -5,8 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BasicTestConnect1.class, BasicTestConnect2_ClientSendData.class, BasicTestConnect3_ServerSendToClient.class, TestClientBasicDisconnect.class, TestServerClientTimeout.class, TestServerDisconnect.class,
-		TestServerDisconnectReconnect.class, TestServerDisconnectReconnectDisonnectReconnectDisconnect.class, TestServerWhenClientDisconnect.class, TestTimeToRemoveFromReceiveQueue.class })
+@SuiteClasses({ BasicTestConnect1.class, BasicTestConnect2_ClientSendData.class,
+		BasicTestConnect3_ServerSendToClient.class, BasicTestConnect4_ClientSendServer5UniquePackets.class,
+		BasicTestConnect5_ServerSendClient5UniquePackets.class, TestClientBasicDisconnect.class,
+		TestServerClientTimeout.class, TestServerDisconnect.class, TestServerDisconnectReconnect.class,
+		TestServerDisconnectReconnectDisonnectReconnectDisconnect.class, TestServerWhenClientDisconnect.class,
+		TestTimeToRemoveFromReceiveQueue.class })
 public class AllTests {
 
 }
